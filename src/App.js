dfd0dbb78595d391be4cc9efe279cb4492e63ca6
Marcus Lo <m._.l@outlook.com>
2021-02-08 00:40:20 +1100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import Logo from './Logo.png'
+import { useEffect, useState } from 'react'
 
-function App() {
+const Button = ({ label, link }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <a
+      target="_blank"
+      rel="noreferrer"
+      style={{
+        textAlign: 'center',
+        padding: 20,
+        margin: 8,
+        borderRadius: 80,
+        background: 'black',
+        color: '#FFC222',
+        fontFamily: 'Montserrat',
+        textDecoration: 'none',
+        textTransform: 'uppercase',
+      }}
+      href={link}
+    >
+      {label}
+    </a>
+  )
 }
 
-export default App;
+function App() {
+  const [prize, setPrize] = useState()
+  useEffect(() => {
+    const merch = ['Pens', 'Hand San', 'Sticky Notes']
+    let looper = setInterval(
+      () => setPrize(merch[Math.floor(Math.random() * merch.length)]),
+      30
+    )
+    setTimeout(function () {
+      clearInterval(looper)
+    }, 3000)
+  }, [])
+  return (
+    <div
+      style={{
+        background: '#FFC222',
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: ' center',
+        alignContent: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div style={{ alignSelf: 'center' }}>
+        <img
+          src={Logo}
+          alt="Enactus UNSW"
+          style={{ width: 240, paddingBottom: 40 }}
+        />
+      </div>
+      <div style={{ fontFamily: 'Montserrat' }}>YOU HAVE WON</div>
+      <div
+        style={{ alignSelf: 'center', fontFamily: 'Montserrat', fontSize: 64 }}
+      >
+        {prize}
+      </div>
+      <div style={{ marginTop: 40, display: 'flex', flexDirection: 'column' }}>
+        <Button label="Facebook" link="https://www.facebook.com/enactusunsw/" />
+        <Button label="Instagram" link="https://instagram.com/enactus.unsw/" />
+        <Button
+          label="LinkedIn"
+          link="https://au.linkedin.com/school/enactus-unsw/"
+        />
+        <Button label="Newsletter" link="http://eepurl.com/gi4Ud9" />
+        <Button label="Website" link="https://enactusunsw.org/" />
+      </div>
+    </div>
+  )
+}
+
+export default App
