5495ed9f87643c856f2abca5b45b049839bd1016
Marcus Lo <m._.l@outlook.com>
2021-02-08 00:48:00 +1100

    fixes

--- src/App.js
+++ src/App.js
@@ -40,8 +40,9 @@ function App() {
     <div
       style={{
         background: '#FFC222',
-        width: '100vw',
-        height: '100vh',
+        width: '100%',
+        height: '100%',
+        minHeight: '100vh',
         display: 'flex',
         flexDirection: 'column',
         alignItems: ' center',
